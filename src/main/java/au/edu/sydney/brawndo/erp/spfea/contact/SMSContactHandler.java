2a9b966de4fa7f849961a169e557c30b2e6008a8
Hamish Croser <hcro4489@uni.sydney.edu.au>
2023-06-01 14:19:41 +1000

    Bulky Contact Method Solution - Chain of Responsibility, Abstract Factory (updated)

---
+++ src/main/java/au/edu/sydney/brawndo/erp/spfea/contact/SMSContactHandler.java
@@ -0,0 +1,27 @@
+package au.edu.sydney.brawndo.erp.spfea.contact;
+
+import au.edu.sydney.brawndo.erp.auth.AuthToken;
+import au.edu.sydney.brawndo.erp.contact.SMS;
+import au.edu.sydney.brawndo.erp.ordering.Customer;
+
+/**
+ * Part of the Chain of Responsibility pattern. This class extends ContactHandler to
+ * act as a Concrete Handler as part of the pattern.
+ *
+ * Part of the static Factory Method pattern. This class acts as the Product that is produced by
+ * the Factory
+ */
+public class SMSContactHandler extends ContactHandler {
+  @Override
+  public boolean sendInvoice(AuthToken token, Customer customer, String data) {
+    if (null != customer) {
+      String smsPhone = customer.getPhoneNumber();
+      if (null != smsPhone) {
+        SMS.sendInvoice(token, customer.getfName(), customer.getlName(), data, smsPhone);
+        return true;
+      }
+    }
+
+    return this.delegateToSuccessor(token, customer, data);
+  }
+}
